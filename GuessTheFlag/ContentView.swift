39ac59811e45e93e3a738e15812486cfabdf7ff1
Camelia Braghes <cameliabraghes@gmail.com>
2022-11-07 14:22:43 +0200

    Remove empty lines

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -4,7 +4,6 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
-    
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     
     var body: some View {
@@ -47,8 +46,6 @@ struct ContentView: View {
         correctAnswer = Int.random(in: 0...2)
     }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
