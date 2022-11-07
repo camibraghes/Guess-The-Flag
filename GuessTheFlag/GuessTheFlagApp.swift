29d816311fa039adf7b4848ca2e1bbd072204a11
Camelia Braghes <cameliabraghes@gmail.com>
2022-11-07 11:38:47 +0200

    Initial setup

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Camelia Braghes on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
