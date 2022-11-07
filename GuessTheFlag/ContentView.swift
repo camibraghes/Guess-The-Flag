b1dd090c8eb061bcbddcd837b31da757e4c892d7
Camelia Braghes <cameliabraghes@gmail.com>
2022-11-07 14:11:51 +0200

    Add logic for correct answer

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    var correctAnswer = Int.random(in: 0...2)
-    
+ 
     var body: some View {
         ZStack {
             Color.blue.ignoresSafeArea()
@@ -14,7 +16,9 @@ struct ContentView: View {
                 
                 VStack {
                     ForEach(0..<3) { flag in
-                        Button {}
+                        Button {
+                            flagTapped(flag)
+                        }
                     label: {
                         Image(countries[flag])
                             .opacity(0.9)
@@ -24,7 +28,19 @@ struct ContentView: View {
             }
         }
     }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Corrrect"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
